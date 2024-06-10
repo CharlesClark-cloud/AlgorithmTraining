306ec66d1af94472d3b5ce4fcb9de128ff5895b9
Charles_clark <1614652415@qq.com>
2024-06-10 18:41:45 +0800

    Day 2

--- Array/src/Code_Link.md
+++ Array/src/Code_Link.md
@@ -1,2 +1,4 @@
 MultiElement - 多数元素
 https://leetcode.cn/problems/majority-element/description/?envType=study-plan-v2&envId=top-interview-150
+MultiElement - 多数元素2
+https://leetcode.cn/problems/majority-element-ii/
--- Array/src/MultiElement.java
+++ Array/src/MultiElement.java
@@ -10,37 +10,34 @@ public class MultiElement {
 
     public static void main(String[] args) {
         int[] a = new int[]{3, 2, 3};
-        (new Solution()).majorityElement(a);
+        (new MultiElement()).majorityElement(a);
     }
-
-}
-class Solution {
-    Solution() {
-    }
-
     public int majorityElement(int[] nums) {
         HashMap map = new HashMap();
-        int maxKey = 0;
-        int maxValue = 0;
-
-        for (int i = 0; i < nums.length; ++i) {
-            if (map.containsKey(nums[i])) {
-                int updateValue = (Integer) map.get(nums[i]);
-                ++updateValue;
-                if (updateValue > maxValue) {
+        int maxKey = 0,maxValue = 0;
+        for(int i =0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){ //元素是否出现过
+                int updateValue = (int) map.get(nums[i]);//获取对应元素的出现次数
+                updateValue++;
+                if(updateValue > maxValue){ //超过当前最多次数的元素，max更新为此元素
                     maxValue = updateValue;
                     maxKey = nums[i];
                 }
+                map.put(nums[i],updateValue);
+            }else {
+                if(maxValue == 0){
+                    maxValue = 1;
+                    maxKey = nums[i];
+                }
+                map.put(nums[i],1); //第一次出现
 
-                map.put(nums[i], updateValue);
-            } else {
-                map.put(nums[i], 1);
             }
         }
-
-        System.out.println("" + maxKey + "-" + maxValue);
-        System.out.println(map);
-        System.out.println(maxKey);
-        return maxKey;
+        if(maxValue> nums.length / 2){
+            return maxKey;
+        }
+        return 0;
     }
+
 }
+
