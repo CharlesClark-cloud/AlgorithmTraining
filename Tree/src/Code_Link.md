d44ab900f01492e1166d8d46f2bf22f37522156b
Charles_clark <1614652415@qq.com>
2024-07-04 22:23:19 +0800

    Day 20

---
+++ Tree/src/Code_Link.md
@@ -0,0 +1,2 @@
+IsSameTree - 相同的二叉树
+https://leetcode.cn/problems/same-tree/?envType=study-plan-v2&envId=top-interview-150
---
+++ Tree/src/IsSameTree.java
@@ -0,0 +1,37 @@
+/**
+ * ClassName: IsSameTree
+ * Package: PACKAGE_NAME
+ */
+
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return judge(p,q);
+
+    }
+    public boolean judge(TreeNode p, TreeNode q){
+        if(p==null&&q==null){
+            //都为空时则树到底，中间的值都想同
+            return true;
+        }
+        if(p==null||q==null||p.val!=q.val){
+            //一边为空了，那么上一个if没过，说明不一样了。或者值不一样了，错了
+            return false;
+        }
+        //最后返回左右子树是否都相同 相同才返回
+        return judge(p.left, q.left) && judge(p.right, q.right);
+    }
+}
