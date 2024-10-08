a9bb0ac9abc33e833aafce851974b730c6803a0b
Charles_clark <1614652415@qq.com>
2024-10-08 15:04:13 +0800

    Day 42

--- Tree/src/LayerPrint.java
+++ Tree/src/LayerPrint.java
@@ -8,6 +8,7 @@ public class LayerPrint{
     //计算二叉树的每层平均值
     //其实就是二叉树的层序遍历
     public  static  List<Double> averageOfLevels(TreeNode root) {
+        //广度优先探索
         List<Double> ansList  = new LinkedList<>();
         Deque<TreeNode> deque = new ArrayDeque<>();
         deque.add(root);
@@ -30,6 +31,36 @@ public class LayerPrint{
             ansList.add(sum/layerSize);
         }
         return ansList;
+    }
+    public static  List<Double> layerPrintDeepFirst(TreeNode root){
+        //深度优先遍历
+        List<Integer> counts = new ArrayList<>();
+        List<Double> sums = new ArrayList<>();
+        int level  = 0;
+        dfs(root,level,counts,sums);
+        List<Double> ans = new ArrayList<>();
+        for (int i = 0; i < counts.size(); i++) {
+            ans.add(sums.get(i)/ counts.get(i));
+        }
+        return  ans;
+
+    }
+    public  static  void  dfs(TreeNode root,int level, List<Integer> counts, List<Double> sums){
+        if(root == null){
+            return;
+        }
+        if(level<sums.size()){
+            // 说明目前所在的层数是已经到达过的层数
+            //直接调整counts，然后计入sums。
+            counts.set(level,counts.get(level)+1);
+            sums.set(level,sums.get(level)+root.val);
+        }else{
+            //说明目前所到的层数是之前没有到过的
+            counts.add(1);
+            sums.add(root.val*1.0);
+        }
+        dfs(root.left,level+1,counts,sums);
+        dfs(root.right, level+1, counts, sums);
     }
 
 
